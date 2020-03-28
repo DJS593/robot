bc18e7b10a9141e9de7e7204d9ba6552b465ff01
DJS <davidstahl@Davids-MacBook-Pro.local>
2020-03-27 20:18:19 -0700

    first round with Roborto

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,36 @@
 var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
+console.log(playerName, playerHealth, playerAttack);
 
-console.log("leaving myself a message");
-console.log(10+10);
-console.log("my favorite player " + playerName);
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+console.log(enemyName, enemyHealth, enemyAttack);
 
-function fight() {
+
+var fight = function() {
   window.alert("The fight has begun!");
+
+  enemyHealth = enemyHealth - playerAttack;
+  console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+
+  if(enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");}
+  else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");}
+
+  playerHealth = playerHealth - enemyAttack;
+  console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+
+  if(playerHealth <= 0) {
+    window.alert(playerName + " has died!");}
+  else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");}
+
+
+
 }
 
 
-// fight();
+fight();
